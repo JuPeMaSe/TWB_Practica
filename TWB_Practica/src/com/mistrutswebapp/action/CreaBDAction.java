777c2d564555788da0917e313a6f733cd29aa617
JP <jpmarquez199@gmail.com>
2015-04-17 20:24:05 +0200

    Añadido la creación de la BD completa con todas las tablas y sus actuales atributos desde la clase CreaBDAction.java

--- TWB_Practica/src/com/mistrutswebapp/action/CreaBDAction.java
+++ TWB_Practica/src/com/mistrutswebapp/action/CreaBDAction.java
@@ -46,13 +46,11 @@ public class CreaBDAction extends Action {
                  
               // pido una conexion
               con=ds.getConnection();
-        
-              // la uso
               //System.out.println(con);
 			  log.info("In LoginAction despues de connection");
          // try {  
               st  = con.createStatement();  
-             // sql = "CREATE TABLE usuarios (user_ID VARCHAR(20), password VARCHAR(12), nombre VARCHAR(100))";
+              //Creamos la tabla Usuario
               sql = "CREATE TABLE Usuario(" +
               		"User_ID VARCHAR(30) NOT NULL PRIMARY KEY," +
               		"	Password VARCHAR(30)," +
@@ -61,14 +59,71 @@ public class CreaBDAction extends Action {
               		"	Fe_Nac DATE," +
               		"	Tfno CHAR(9)," +
               		"	Email VARCHAR(60)," +
-              		"	UserType VARCHAR(3))";
+              		"	UserType VARCHAR(3));";
               st.executeUpdate(sql);
-              //Borramos los usuarios que existiesen en la tabla usuarios  
+            //  log.info("In LoginAction despues de crear tabla Usuario");
+              //Creamos la tabla Perfil
+              sql="CREATE TABLE Perfil(" +
+              		"	Profile_ID INTEGER NOT NULL PRIMARY KEY," +
+              		"	Pdf BLOB," +
+              		"	Fotografia BLOB," +
+              		"	Direccion VARCHAR(40)," +
+              		"	Localidad VARCHAR(40)," +
+              		"	Provincia VARCHAR(40)," +
+              		"	Pais VARCHAR(40)," +
+              		"	Cont_MeGusta INTEGER," +
+              		"	User_ID VARCHAR(30)" +
+              		");";
+              st.executeUpdate(sql);
+             // log.info("In LoginAction despues de crear tabla Perfil");
+             // Creamos la table Tecnologia
+             sql = "CREATE TABLE Tecnologia(	" +
+              		"Tecnologia_ID INTEGER NOT NULL PRIMARY KEY," +
+              		"	Nombre_Tec VARCHAR(50)" +
+              		");";
+              st.executeUpdate(sql);
+              //log.info("In LoginAction despues de crear tabla Tecnologia");
+              //Creamos la tabla Perfil-Tec
+             sql= "CREATE TABLE Perfil_Tec(" +
+              		"	Tecnologia_ID INTEGER," +
+              		"	Profile_ID INTEGER" +
+              		");";
+              st.executeUpdate(sql);
+              //log.info("In LoginAction despues de crear tabla Perfil_Tec");
+              //Creamos la tabla Titulacion
+              sql="CREATE TABLE Titulacion(" +
+              		"	Titulacion_ID INTEGER NOT NULL PRIMARY KEY," +
+              		"	Nombre_Tit VARCHAR(50)" +
+              		");";
+              st.executeUpdate(sql);
+              //log.info("In LoginAction despues de crear tabla Titulación");
+              //Creamos la tabla Perfil_Tit
+              sql="CREATE TABLE Perfil_Tit(" +
+              		"	Titulacion_ID INTEGER," +
+              		"	Profile_ID INTEGER" +
+              		");";
+              st.executeUpdate(sql);
+              //log.info("In LoginAction despues de crear tabla Perfil_Tit");
+              //Creamos la tabla Experiencia
+              sql="CREATE TABLE Experiencia(" +
+              		"	Exp_ID INTEGER NOT NULL PRIMARY KEY," +
+              		"	Empresa VARCHAR(60)," +
+              		"	Cargo VARCHAR(50)," +
+              		"	A_Inicio INTEGER," +
+              		"	A_Fin INTEGER," +
+              		"	Profile_ID INTEGER" +
+              		");";
+              st.executeUpdate(sql);
+              //log.info("In LoginAction despues de crear tabla Experiencia");
+              
+              //Borramos los usuarios que existiesen en la tabla Usuario  
               st.executeUpdate("DELETE FROM Usuario");  
            // Varios usuarios de prueba  
-              st.executeUpdate("INSERT INTO Usuario (User_ID, Password, Nombre) VALUES ('jpms', '4367',  'Juan Pedro Marquez Sevilla')");
-              st.executeUpdate("INSERT INTO usuarios (User_ID, Password, Nombre) VALUES ('ja', '5367',  'Jose Angel')");  
-      			st.executeUpdate("INSERT INTO usuarios (User_ID, Password, Nombre) VALUES ('jj', '6367',  'Juan Jose')");  
+              st.executeUpdate("INSERT INTO Usuario (User_ID, Password, Nombre, Apellidos, Tfno, Email, UserType) " +
+              		"VALUES ('jpms', '4367',  'Juan Pedro', 'Marquez Sevilla','', '','adm')");
+              //st.executeUpdate("INSERT INTO Usuario (User_ID, Password, Nombre) VALUES ('jpms', '4367',  'Juan Pedro')");
+              st.executeUpdate("INSERT INTO Usuario (User_ID, Password, Nombre) VALUES ('ja', '5367',  'Jose Angel')");  
+      			st.executeUpdate("INSERT INTO Usuario (User_ID, Password, Nombre) VALUES ('jj', '6367',  'Juan Jose')");  
 	// Mostramos por pantalla todos los usuarios de la tabla  
             rst1 = st.executeQuery("SELECT * FROM Usuario");  
             while (rst1.next()){  
@@ -82,9 +137,9 @@ public class CreaBDAction extends Action {
              st.close();  
               con.close();  
           } catch (Exception ex){  
-              // La tabla ya existia  
+              log.error("BD no creada por --> "+ ex.getMessage());
           }finally{  
-    
+        	  
         
           }
           log.info("In CreaBDAction --> Base de datos creada");
